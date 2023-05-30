ad34efeb151a7087534cd9ff1e5ebe83fda4550c
7CYJ <jojo3528@gmail.net>
2023-05-30 16:00:37 +0900

    초기 설정 완료

---
+++ src/main/java/com/lesson/project/LessonTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lesson.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonTeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonTeamProjectApplication.class, args);
+	}
+
+}
